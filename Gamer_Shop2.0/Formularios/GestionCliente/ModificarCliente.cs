ec8ec1711bd771bb2afea77b0fe8dde76d80e678
Morena Salinas Sanette <moresalinass08@gmail.com>
2024-09-20 12:54:51 -0300

    Validaciones ModificarCliente incorporadas

--- Gamer_Shop2.0/Formularios/GestionCliente/ModificarCliente.cs
+++ Gamer_Shop2.0/Formularios/GestionCliente/ModificarCliente.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -96,16 +97,171 @@ namespace Gamer_Shop2._0.Formularios.GestionCliente
 
         private void BReturnToBack_Click(object sender, EventArgs e)
         {
-            // Crear una nueva instancia de ListaCLiente
-            ListaCliente listCl = new ListaCliente();
-            listCl.TopLevel = false;
+            DialogResult = MessageBox.Show("Está seguro que desea volver? Se perderán los cambios realizados", "Modificación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (DialogResult == DialogResult.Yes)
+            {
+                // Crear una nueva instancia de ListaCLiente
+                ListaCliente listCl = new ListaCliente();
+                listCl.TopLevel = false;
 
-            // Limpiar el panel actual y volver al anterior formulario.
-            PanelContainer.Controls.Clear();
-            PanelContainer.Controls.Add(listCl);
-            listCl.PanelContainer = PanelContainer;
-            listCl.Show();
-            this.Close();
+                // Limpiar el panel actual y volver al anterior formulario.
+                PanelContainer.Controls.Clear();
+                PanelContainer.Controls.Add(listCl);
+                listCl.PanelContainer = PanelContainer;
+                listCl.Show();
+                this.Close();
+            }
+        }
+
+        private void TNombreCl_Validating(object sender, CancelEventArgs e)
+        {
+            if (this != null)
+            {
+                if (TNombreCl.Texts.Length >= 100)
+                {
+                    e.Cancel = true;
+                    TBValidacion.Visible = true;
+
+                }
+                else
+                {
+                    TBValidacion.Visible = false;
+                }
+            }
+        }
+
+        private void TApellidoCl_Validating(object sender, CancelEventArgs e)
+        {
+            if (this != null)
+            {
+                if (TApellidoCl.Texts.Length >= 100)
+                {
+                    e.Cancel = true;
+                    TBValidacion2.Visible = true;
+
+                }
+                else
+                {
+                    TBValidacion2.Visible = false;
+                }
+            }
+        }
+
+        private void TTelefonoCl_Validating(object sender, CancelEventArgs e)
+        {
+            if (this != null)
+            {
+                long number;
+                if (!long.TryParse(TTelefonoCl.Texts, out number))
+                {
+                    e.Cancel = true;
+                    TBValidacion3.Visible = true;
+
+                }
+                else
+                {
+                    TBValidacion3.Visible = false;
+                }
+            }
+        }
+
+        private void TEmailCl_Validating(object sender, CancelEventArgs e)
+        {
+            if (this != null)
+            {
+                string patronCorreo = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+                bool esValido = Regex.IsMatch(TEmailCl.Texts, patronCorreo);
+
+                if (!esValido)
+                {
+                    e.Cancel = true;
+                    TBValidacion4.Visible = true;
+
+                }
+                else
+                {
+                    TBValidacion4.Visible = false;
+                }
+            }
+        }
+
+        private void TNombreCl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool escontrol = Char.IsControl(e.KeyChar);
+            bool longitud = TNombreCl.Texts.Trim().Length < 100;
+
+            if (longitud || escontrol)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TApellidoCl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool escontrol = Char.IsControl(e.KeyChar);
+            bool longitud = TApellidoCl.Texts.Trim().Length < 100;
+
+            if (longitud || escontrol)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TTelefonoCl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool escontrol = Char.IsControl(e.KeyChar);
+            bool longitud = TTelefonoCl.Texts.Trim().Length < 20;
+
+            if (longitud || escontrol)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TEmailCl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool escontrol = Char.IsControl(e.KeyChar);
+            bool longitud = TEmailCl.Texts.Trim().Length < 50;
+
+            if (longitud || escontrol)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.AutoValidate = AutoValidate.EnablePreventFocusChange;
+        }
+
+        private void BModificarUs_Click(object sender, EventArgs e)
+        {
+            if (TNombreCl.Texts != string.Empty && TApellidoCl.Texts != string.Empty && TTelefonoCl.Texts != string.Empty && TEmailCl.Texts != string.Empty)
+            {
+                MessageBox.Show("Usuario modificado con éxito", "Modificación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Debe completar todos los campos para modificar", "Modificación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
         }
     }
 }
